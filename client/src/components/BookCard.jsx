10fc3108c624b9958a6d485def94535b3ae7f7c7
[Ashley Braxton] <ashley.braxton25@gmail.com>
2023-09-04 06:49:09 -0400

    geting the books in the cart to render

--- client/src/components/BookCard.jsx
+++ client/src/components/BookCard.jsx
@@ -14,7 +14,7 @@ const BookCard = () => {
         condition:
           target.parentElement.querySelector('#bookCondition').textContent,
         listedPrice:
-          target.parentElement.querySelector('#bookCondition').textContent,
+          target.parentElement.querySelector('#bookPrice').textContent,
       };
       const productString = JSON.stringify(product);
       cartProducts.push(productString);
@@ -34,7 +34,12 @@ const BookCard = () => {
           </h5>
           <p id="bookCondition">{data.condition}</p>
           <p id="bookPrice">${data.listedPrice}</p>
-          <button
+          <button 
+          onClick={(event) => {
+            if (event.target === event.currentTarget) {
+            addToCart(event);
+            }
+            }}
             type="button"
             className="text-white bg-forestfront-50 hover:bg-forestback-50 focus:ring-4 focus:outline-none focus:ring-bg-forestback-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-forestback-600 dark:hover:bg-forestback-700 dark:focus:ring-forestback-800"
           >
@@ -47,11 +52,6 @@ const BookCard = () => {
             >
               <path d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
             </svg>
-            {(event) => {
-              if (event.target === event.currentTarget) {
-                addToCart(event);
-              }
-            }}
             Add to Cart
           </button>
         </Card>
--- client/src/components/cart.jsx
+++ client/src/components/cart.jsx
@@ -1,66 +1,65 @@
-import React, { useState, useEffect, useContext, useNavigate } from 'react';
+import React, { useState, useEffect } from 'react';
 import { loadStripe } from '@stripe/stripe-js';
 
 const stripePromise = loadStripe('pk_test_51NimDjDT6tfiNPpdPw5gNPITk6ta8X2oBoWNOUuIEFPtGR3gOO0HzXxxKnBE9mw6pUIvNXhcxZD66s28hbmU0hx500MBa74YtM');
 
-const CartItem = ({ data = { title: '', price: 0, quantity: 0 }}) => {
+const CartItem = (data) => {
+  const product = JSON.parse(data.product);
+  const listedPrice = parseFloat(product.listedPrice.replace('$', ''));
+  const price = parseInt(listedPrice);
+
   return (
-    <div key={data._id}>
-      <h3>{data.title}</h3>
-      <p>${data.price}</p>
+    <div id='cart-item' key={product._id}>
+      <h3 id='cart-book-title'>{product.title}</h3>
+      <p id='cart-book-price'>${price}</p>
     </div>
   );
 };
 
-const Cart = () => {
-
+const Cart = (props) => {
   const [cartProducts, setCartProducts] = useState([]);
-  
-  useEffect(() => {
+  const [total, setTotal] = useState(0);
+  const [totalPrice, setTotalPrice] = useState(0);
+
   const getCartProducts = async () => {
-  const cartProductsFromLocalStorage = localStorage.getItem('cart');
-  if (cartProductsFromLocalStorage) {
-  setCartProducts(JSON.parse(cartProductsFromLocalStorage));
-  }
+    const cartProductsFromLocalStorage = localStorage.getItem('cart');
+    if (cartProductsFromLocalStorage) {
+      const parsedCartProducts = JSON.parse(cartProductsFromLocalStorage);
+      setCartProducts(parsedCartProducts);
+    }
   };
-  
-  getCartProducts();
+
+  useEffect(() => {
+    getCartProducts();
   }, []);
-  
-  // ...
-  
+
   function submitCheckout() {
-  context.submitCheckout();
+    // Your checkout logic here
   }
-  
-  // ...
-  
+
   return (
-  <div className='cart'>
-  <h2>Your Cart</h2>
-  {cartProducts.length ? (
-  <div>
-  {cartProducts.map((data) => (
-  <CartItem product={data} />
-  ))}
-  <div className='flex-row space-between'>
-  <strong>
-  {/* Total: ${calculateTotal()} */}
-  </strong>
-  <button onClick={submitCheckout}>Checkout</button>
-  {/* <button onClick={continueShopping}>Continue Shopping</button> */}
-  </div>
-  </div>
-  ) : (
-  <div>
-  <h3>
-  You haven't added anything to your cart yet!
-  </h3>
-  {/* <button onClick={continueShopping}>Start Shoping Now!</button> */}
-  </div>
-  )}
-  </div>
+    <div className='cart'>
+      <h2 id='cart-title'>Your Cart</h2>
+      {cartProducts.length ? (
+        <div>
+          {cartProducts.map((data) => (
+            <CartItem product={data} totalPrice={totalPrice} />
+          ))}
+          <div className='flex-row space-between'>
+            <div>
+              <button id='checkout-btn' onClick={submitCheckout}>Checkout</button>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <h3>
+            You haven't added anything to your cart yet!
+          </h3>
+        </div>
+      )}
+    </div>
   );
-  };
-  
-  export default Cart;
+};
+
+export default Cart;
